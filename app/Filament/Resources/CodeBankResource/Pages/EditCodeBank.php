6d2128634cf79436062f1940bf0bf8f5e1bb76e9
amordz72 <dzamor72@gmail.com>
2022-04-14 00:14:39 +0100

    end bank

---
+++ app/Filament/Resources/CodeBankResource/Pages/EditCodeBank.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\CodeBankResource\Pages;
+
+use App\Filament\Resources\CodeBankResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCodeBank extends EditRecord
+{
+    protected static string $resource = CodeBankResource::class;
+}
---
+++ app/Filament/Resources/CodeBankResource/Pages/ListCodeBanks.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\CodeBankResource\Pages;
+
+use App\Filament\Resources\CodeBankResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListCodeBanks extends ListRecords
+{
+    protected static string $resource = CodeBankResource::class;
+}
