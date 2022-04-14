caf6cf38cdfe554f0c43f6ee13ee65fbd5b67234
amordz72 <dzamor72@gmail.com>
2022-04-14 06:28:15 +0100

    accordionExample

--- resources/views/livewire/code/index.blade.php
+++ resources/views/livewire/code/index.blade.php
@@ -5,39 +5,24 @@
 
 
                 @foreach ($codes as $code)
-
-
-
-
-
-
-
-
-
-
-
-
-
-                <div class="accordion" id="accordionExample">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingOne">
-                            <button class="accordion-button" type="button"
-                             data-bs-toggle="collapse" data-bs-target="#collapseOne{{ ($code['id'])}}" aria-expanded="true" aria-controls="collapseOne">
-                                {{ ($code['short']) }}
-                            </button>
-                        </h2>
-                        <div id="collapseOne{{ ($code['id'])}}" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                            <textarea class="accordion-body form-control" rows="10">
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingOne">
+                                <button class="accordion-button text-dark fw-bold" type="button"
+                                    data-bs-toggle="collapse" data-bs-target="#collapseOne{{ $code['id'] }}"
+                                    aria-expanded="true" aria-controls="collapseOne">
+                                    {{ $code['short'] }}
+                                </button>
+                            </h2>
+                            <div id="collapseOne{{ $code['id'] }}" class="accordion-collapse collapse show"
+                                aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <textarea class="accordion-body form-control" rows="10">
                                 {{ ltrim($code['body']) }}
                             </textarea>
+                            </div>
                         </div>
+
                     </div>
-
-                </div>
-
-
-
-
                 @endforeach
 
             </div>
