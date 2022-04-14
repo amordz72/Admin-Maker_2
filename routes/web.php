a7590f5a8b7fa6fd14bfae2dfda4aecaababb969
amordz72 <dzamor72@gmail.com>
2022-04-14 04:11:51 +0100

    {{ route('code.index') }}

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ use App\Http\Livewire as lw;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Route::get('/code',lw\Code\Index::class);
+Route::get('/code',lw\Code\Index::class)->name('code.index');
