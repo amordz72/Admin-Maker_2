c80c399b134a172a0ced9ae22948b52af191b16a
amordz72 <dzamor72@gmail.com>
2022-04-15 15:57:40 +0100

    rename code

--- resources/views/livewire/code/index.blade.php
+++ resources/views/livewire/code/index.blade.php
@@ -62,7 +62,7 @@
                     </li>
                     <li class="nav-item" role="presentation">
                         <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile"
-                            type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
+                            type="button" role="tab" aria-controls="profile" aria-selected="false">Code</button>
                     </li>
                     <li class="nav-item" role="presentation">
                         <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact"
